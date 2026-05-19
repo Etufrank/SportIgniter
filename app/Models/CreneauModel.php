8600925da9699c55c5629d0e7002427bf753b5a3
TsitoKely07 <tsitoratsifa@gmail.com>
2026-05-19 11:38:07 +0300

    Ito le model

--- app/Models/CreneauModel.php
+++ app/Models/CreneauModel.php
@@ -12,4 +12,14 @@ class CreneauModel extends Model {
                     ->join('ressources', 'ressources.id = creneaux.ressource_id')
                     ->findAll();
     }
+
+    public function getCreneauxFuturs(){
+        return $this->select('creneaux.*, ressources.nom as ressource_nom, ressources.type as ressource_type, ressources.description as ressource_desc')
+                                         ->join('ressources', 'ressources.id = creneaux.ressource_id')
+                                         ->where('creneaux.actif', 1)
+                                         ->where('creneaux.date_debut >=', date('Y-m-d H:i:s'))
+                                         ->orderBy('creneaux.date_debut', 'ASC')
+                                         ->findAll();
+    }
+    
 }
